3390652d1562d8c2464d8e8733a84daf67289c7c
Benoit Prigent <benoit.prigent@grenoble-em.com>
2018-02-19 18:02:31 +0100

    add hasmany relation to the model performance

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,5 +1,6 @@
 class Performance < ApplicationRecord
   belongs_to :artist, class_name: "User", foreign_key: :user_id
+  has_many :bookings
   validates :description, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
