37c5f0b8ffa31bb8f222657803a8539af8bf675f
Alexis Pipieri <alexis.pipieri@gmail.com>
2018-02-19 17:41:36 +0100

    add columns to users and add validations to users and bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,4 @@
 class Booking < ApplicationRecord
   belongs_to :performance
-  belongs_to :user
+  belongs_to :customer, class_name: "User", foreign_key: :user_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :username, presence: true, uniqueness: true
+  has_many :performances
+  has_many :bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
