b1068ec5d88073a90acb3c4bce01175a06eec802
hugo le bihan <hugo_lb@hotmail.fr>
2018-02-20 13:04:43 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,13 @@ User.destroy_all
 Performance.destroy_all
 
 
-user1 = user.create!(username: "Maximilien", avatar:"http://bit.ly/2EF3rJz", is_artist: true, photo: "http://bit.ly/2ojAA2x",location: "New York", bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.")
-user2 = user.create!(username: "Tanguy", avatar:"http://bit.ly/2Hx6P7l", is_artist: true, photo: "http://bit.ly/2EFWJmR" location: "Paris", bio:  "ConsecteturLorem ipsum dolor sit amet, adipisicing elit.")
-user3 = user.create!(username: "Airpod", avatar:"http://bit.ly/2C9aBEq", is_artist: false, location: "l'Eldorado", bio: "Adipisicing lorem ipsum dolor sit amet, consectetur elit.")
+user1 = User.create!(username: "Maximilien", avatar:"http://bit.ly/2EF3rJz", email: "email@email.com", password: "123456", is_artist: true, photos: "http://bit.ly/2ojAA2x",location: "New York", bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.")
+user2 = User.create!(username: "Tanguy", avatar:"http://bit.ly/2Hx6P7l", email: "email2@email.com", password: "123456",  is_artist: true, photos: "http://bit.ly/2EFWJmR", location: "Paris", bio:  "ConsecteturLorem ipsum dolor sit amet, adipisicing elit.")
+user3 = User.create!(username: "Airpod", avatar:"http://bit.ly/2C9aBEq", email: "email3@email.com", password: "123456",  is_artist: false, location: "l'Eldorado", bio: "Adipisicing lorem ipsum dolor sit amet, consectetur elit.")
 
-performance1 = performance.create!(description: "Wall painting", price: "200$", description: "aliquid quaerat quibusdam.", user: user1)
-performance2 = performance.create!(description: "door painting", price: "100$", description: " quaerat aliquid quibusdam.", user: user2)
-performance3 = performance.create!(description: "ceiling painting", price: "300$", description: "quibusdam aliquid quaerat .", user: user1)
+performance1 = Performance.create!(description: "Wall painting", price: 200, user_id: user1.id)
+performance2 = Performance.create!(description: "door painting", price: 100, user_id: user2.id)
+performance3 = Performance.create!(description: "ceiling painting", price: 300, user_id: user1.id)
 
 
 
