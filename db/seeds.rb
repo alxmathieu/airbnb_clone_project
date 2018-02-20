ffab9318f236685f56b580812b5c8da100a48073
hugo le bihan <hugo_lb@hotmail.fr>
2018-02-20 16:28:33 +0100

    add card

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,8 @@ User.destroy_all
 Performance.destroy_all
 
 
-user1 = User.create!(username: "Maximilien", avatar:"http://bit.ly/2EF3rJz", email: "email@email.com", password: "123456", is_artist: true, photos: "http://bit.ly/2ojAA2x",location: "New York", bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.")
-user2 = User.create!(username: "Tanguy", avatar:"http://bit.ly/2Hx6P7l", email: "email2@email.com", password: "123456",  is_artist: true, photos: "http://bit.ly/2EFWJmR", location: "Paris", bio:  "ConsecteturLorem ipsum dolor sit amet, adipisicing elit.")
+user1 = User.create!(username: "Maximilien", avatar:"http://bit.ly/2EF3rJz", email: "email@email.com", password: "123456", is_artist: true, photos: ["http://bit.ly/2ojAA2x"],location: "New York", bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.")
+user2 = User.create!(username: "Tanguy", avatar:"http://bit.ly/2Hx6P7l", email: "email2@email.com", password: "123456",  is_artist: true, photos: ["http://bit.ly/2EFWJmR"], location: "Paris", bio:  "ConsecteturLorem ipsum dolor sit amet, adipisicing elit.")
 user3 = User.create!(username: "Airpod", avatar:"http://bit.ly/2C9aBEq", email: "email3@email.com", password: "123456",  is_artist: false, location: "l'Eldorado", bio: "Adipisicing lorem ipsum dolor sit amet, consectetur elit.")
 
 performance1 = Performance.create!(description: "Wall painting", price: 200, user_id: user1.id)
