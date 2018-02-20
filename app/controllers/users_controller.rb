63d6445717f1911dfee7a4a1020acc0f011af055
Benoit Prigent <benoit.prigent@grenoble-em.com>
2018-02-20 17:45:22 +0100

    add a form edition to the bio

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to edit_user_path
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:bio)
+  end
+
 end
